52746357b0ca63daec5282be62a70f22f7d9b86f
jwilen <jochum.wilen@gmail.com>
2021-06-05 15:00:24 +0300

    Ex00 with test

--- D01/ex00/main.swift
+++ D01/ex00/main.swift
@@ -5,12 +5,31 @@
 //  Created by Jochum Wilen on 6/5/21.
 //
 
-func test()  {
-    print("\nColours:\n")
+func test()
+{
+    print("\nColours:")
     let allColourTab = Color.allColors
-    for eachcolor in allColourTab {
+    for eachcolor in allColourTab
+    {
         print("Name: \(eachcolor), value: \(eachcolor.rawValue)")
     }
+    print("\nValues:")
+    let allValueTab = Value.allValues
+    for eachvalue in allValueTab
+    {
+        print("Name: \(eachvalue), value: \(eachvalue.rawValue)")
+    }
+    print("\nThe Deck contains the following cards:")
+    var i = 0;
+    for eachcard in allColourTab
+    {
+        for eachnbr in allValueTab
+        {
+        print("Color: \(eachcard.rawValue), Value: \(eachnbr.rawValue)")
+            i += 1
+        }
+    }
+    print("Number of cards in the deck: \(i)" )
 }
 
 test()
