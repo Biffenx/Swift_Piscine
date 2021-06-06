c01fc93e42f4ecda2265dca99f733539f46a3c3b
jwilen <jochum.wilen@gmail.com>
2021-06-06 09:31:39 +0300

    ex02 done

--- D01/ex02/main.swift
+++ D01/ex02/main.swift
@@ -4,33 +4,35 @@
 //
 //  Created by Jochum Wilen on 6/5/21.
 //
-
-let card1 : Card = Card(color: Color.spades, value: Value.Eight)
-let card2 : Card = Card(color: Color.clubs, value: Value.Ace)
-let card3 : Card = Card(color: Color.hearts, value: Value.Four)
-let card4 : Card = Card(color: Color.diamonds, value: Value.King)
-
-print(card1)
-print("Cards we have:")
-print("Card 1 is: \(card1)")
-print("Card 2 is: \(card2)")
-print("Card 3 is: \(card3)")
-print("Card 4 is: \(card4)")
-print("\n *** Test for override function ***")
-print("Test if Card 1 is equal to Card 2")
-print(card1.isEqual(card2))
-print("Test if Card 1 is equal to Card 3")
-print(card1.isEqual(card3))
-print("Test if Card 1 is equal to Card 4")
-print(card1.isEqual(card4))
-print("Test if Card 1 is equal to Card 1")
-print(card1.isEqual(card1))
-print("\n *** Test for overload operator ***")
-print("Test if Card 1 is equal to Card 2")
-print(card1 == card2)
-print("Test if Card 1 is equal to Card 3")
-print(card1 == card3)
-print("Test if Card 1 is equal to Card 4")
-print(card1 == card4)
-print("Test if Card 1 is equal to Card 1")
-print(card1 == card1)
+func test ()
+{
+    print("*** Test to print all Spades ***")
+    for allSpades in Deck.allSpades
+    {
+        print(allSpades)
+    }
+    print("\n*** Test to print all Diamonds ***")
+    for allDiamonds in Deck.allDiamonds
+       {
+           print(allDiamonds)
+       }
+    print("\n*** Test to print all Hearts ***")
+    for allHearts in Deck.allHearts
+       {
+           print(allHearts)
+       }
+    print("\n*** Test to print all Clubs ***")
+    for allClubs in Deck.allClubs
+       {
+           print(allClubs)
+       }
+    print("\n*** Test to print all Cards ***")
+    var i = 0
+    for allCards in Deck.allCards
+       {
+           print(allCards)
+            i += 1
+       }
+    print("Cards of game: \(i)")
+}
+test()
