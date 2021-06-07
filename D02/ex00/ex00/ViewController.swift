266b3c8af99fcb136271d46fcd23e24c5f74a944
jwilen <jochum.wilen@gmail.com>
2021-06-07 18:26:44 +0300

    Ex00 on Day02

---
+++ D02/ex00/ex00/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Jochum Wilen on 6/7/21.
+//  Copyright © 2021 Jochum Wilen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+
+
+}
+
