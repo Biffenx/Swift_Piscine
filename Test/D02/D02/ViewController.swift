9cfdd774d852e3c3e6a6a5494abc53c27cf41c11
jwilen <jochum.wilen@gmail.com>
2021-06-07 08:12:53 +0300

    piscine video from d02

---
+++ Test/D02/D02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  D02
+//
+//  Created by Jochum Wilen on 6/7/21.
+//  Copyright © 2021 Jochum Wilen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
