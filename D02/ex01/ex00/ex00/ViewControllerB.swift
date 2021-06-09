46c8c4b5fd3e892d037394eb19d3aabeddf8828d
Jocke Wilen <jochum.wilen@gmail.com>
2021-06-09 12:03:36 +0300

    working from home, added the done button and making a print function

--- D02/ex01/ex00/ex00/ViewControllerB.swift
+++ D02/ex01/ex00/ex00/ViewControllerB.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewControllerB: UIViewController {
 
+  
+    
+    @IBOutlet weak var rip_description: UITextView!
+    @IBOutlet weak var rip_name: UITextField!
+    @IBOutlet weak var rip_calender: UIDatePicker!
+    
+    
+    @IBAction func doneBtn(_ sender: Any) {
+        if let name = rip_name.text {
+            print("Name: \(name)")
+        }
+        if let description = rip_description.text {
+            print("Description: \(description)")
+        }
+        let date = DateFormatter()
+        date.dateFormat = "dd MMMM yyyy"
+        let formatDate = date.string(from: rip_calender.date)
+        print("Date: \(formatDate)")
+    }
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-
+   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 //         Get the new view controller using segue.destination.
 //         Pass the selected object to the new view controller.
